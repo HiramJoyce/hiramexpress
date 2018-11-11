82a534844c5422e1da9a1d07bf1bd42b67f7001d
曹海亮 <1050588656@qq.com>
2018-11-12 01:15:47 +0800

    定时任务出现bug，加了一些日志进行监控

--- src/main/java/com/hiramexpress/utils/ScheduledTask.java
+++ src/main/java/com/hiramexpress/utils/ScheduledTask.java
@@ -29,17 +29,25 @@ public class ScheduledTask {
     }
 
     @Scheduled(cron = "0 0 0 * * ?")
+//    @Scheduled(fixedRate = 10000)
     public void logTime() {
+        logger.info("");
+        logger.info("=====================    执行定时任务    ===================");
+        Date today = new Date();
+        logger.info("    ==== 当前日期 ：" + today);
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
+        calendar.setTime(today);
         calendar.add(Calendar.DAY_OF_MONTH, -1);
         Date beforeDay = calendar.getTime();    // 获取前一天日期
+        logger.info("    ==== 昨天日期 ：" + beforeDay);
 
         String redisKey = "checkNum_" + new SimpleDateFormat("yyyyMMdd").format(beforeDay);    // eg: checkNum_20181107
+        logger.info("    ==== redisKey ：" + redisKey);
         int checkNum = Integer.parseInt(StringUtils.isEmpty(redisService.get(redisKey)) ? "0" : redisService.get(redisKey));
-        logger.info("===>>> 昨日服务： " + checkNum + " 次。");
+        logger.info("    ==== 昨日 " + beforeDay +  " 服务：" + checkNum + " 次。");
         Record record = recordService.getRecordByDate(beforeDay);
         if (record == null) {
+            logger.info("    =============== null -> insert 数据 ============");
             record = new Record();
             record.setRecordDate(beforeDay);
             record.setRecordTimes(checkNum);
@@ -48,8 +56,11 @@ public class ScheduledTask {
                 redisService.set("historyCount", recordService.allTimes() + "", 24 * 60 * 60L);
             }
         } else {    // 更新旧数据
+            logger.info("    ============== not null -> update 数据 ==========");
             record.setRecordTimes(checkNum);
             recordService.updateRecord(record);
         }
+        logger.info("=====================    定时任务完成    ===================");
+        logger.info("");
     }
 }
